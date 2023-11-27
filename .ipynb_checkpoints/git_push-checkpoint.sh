19a3c91be305418b39bc0b12578adef8e678300f
jadikanskywalker <awesomejaden.hicks@gmail.com>
2023-11-27 21:30:21 +0000

    New commit

--- .ipynb_checkpoints/git_push-checkpoint.sh
+++ .ipynb_checkpoints/git_push-checkpoint.sh
@@ -1,12 +1,12 @@
 git config --global user.email "awesomejaden.hicks@gmail.com"
 git config --global user.name "jadikanskywalker"
 
-curl -u "jadikanskywalker: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" https://github.com/jadikanskywalker/mariobros.git
+curl -H "Authorization: token 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" https://github.com/jadikanskywalker/mariobros.git
 
 
 git add -f .
 git commit -m "New commit"
 
-git remote add origin https://github.com/jadikanskywalker/mariobros.git
+git remote add origin https://jadikanskywalker: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@github.com/jadikanskywalker/mariobros.git
 git branch -M main
 git push -u origin main
